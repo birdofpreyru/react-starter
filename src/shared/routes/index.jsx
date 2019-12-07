ea2ead1452ad3a6a5faefee8f4cd18cdd0e1fa05
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2019-12-07 02:10:40 +0100

    Update of dependencies

--- src/shared/routes/index.jsx
+++ src/shared/routes/index.jsx
@@ -1,17 +1,40 @@
 /**
  * Root router of the app.
  */
+/* eslint-disable react/jsx-props-no-spreading */
+
+import path from 'path';
 
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
+import { CodeSplit, Throbber, webpack } from '@dr.pogodin/react-utils';
+
 import Error404Page from 'components/Error404Page';
-import ExampleRoutes from './examples';
+
+function Examples(props) {
+  return (
+    <CodeSplit
+      chunkName="react-examples"
+      getComponentAsync={
+        () => import(/* webpackChunkName: 'react-examples' */ './examples')
+      }
+      getComponentServer={
+        () => {
+          const p = webpack.resolveWeak('./examples');
+          return webpack.requireWeak(path.resolve(__dirname, p));
+        }
+      }
+      placeholder={Throbber}
+      {...props}
+    />
+  );
+}
 
 export default function Routes() {
   return (
     <Switch>
-      <Route component={ExampleRoutes} path="/" />
+      <Route path="/" component={Examples} />
       <Error404Page />
     </Switch>
   );
