37b67c99dbc9f7fe074e3735d7729a68ad65f84d
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2020-04-13 18:09:00 +0200

    Update of react-utils

--- src/shared/pages/examples/Tooltip/index.jsx
+++ src/shared/pages/examples/Tooltip/index.jsx
@@ -9,14 +9,20 @@ import {
   WithTooltip,
 } from '@dr.pogodin/react-utils';
 
-import './styles.scss';
+import styles from './styles.scss';
 
 export default function TooltipExample({ match }) {
   const buttons = [];
   for (let i = 0; i < 100; i += 1) {
     buttons.push((
       <div key={i}>
-        <WithTooltip tip="Just a sample button with tooltip!">
+        <WithTooltip
+          theme={{
+            arrow: styles.arrow,
+            tooltip: styles.tooltip,
+          }}
+          tip="Just a sample button with tooltip!"
+        >
           <Button>A Button</Button>
         </WithTooltip>
       </div>
