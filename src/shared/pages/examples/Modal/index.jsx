ba941dbf004d88bf60bd7829f7a3f03ba67866f3
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2020-05-15 01:13:39 +0200

    Updates of Modal example page

--- src/shared/pages/examples/Modal/index.jsx
+++ src/shared/pages/examples/Modal/index.jsx
@@ -5,6 +5,7 @@
 import React, { useState } from 'react';
 import {
   Button,
+  Input,
   Link,
   Modal,
   PageLayout,
@@ -14,6 +15,8 @@ import {
 
 export default function ModalExample({ match }) {
   const [show, setShow] = useState(false);
+  const [text, setText] = useState('');
+  const [text2, setText2] = useState('');
   return (
     <PageLayout>
       <Link to={url.parent(match.url)}>&lArr; Content</Link>
@@ -21,10 +24,21 @@ export default function ModalExample({ match }) {
       <Button onClick={() => setShow(true)}>
         Show Modal
       </Button>
+      <Input
+        label="Sample Managed Input"
+        onChange={(e) => setText2(e.target.value)}
+        value={text2}
+      />
       {
         show ? (
           <Modal onCancel={() => setShow(false)}>
-            This is a sample modal.
+            <div>This is a sample modal.</div>
+            <Input label="Sample Non-Managed Input" />
+            <Input
+              label="Sample Managed Input"
+              onChange={(e) => setText(e.target.value)}
+              value={text}
+            />
           </Modal>
         ) : null
       }
