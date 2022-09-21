9c360c9fd53e533374fccaa055e057c99bae8bbe
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2022-09-21 23:59:34 +0200

    Minor improvements & react-utils@1.20.1

--- src/shared/pages/Error404/index.jsx
+++ src/shared/pages/Error404/index.jsx
@@ -1,13 +1,13 @@
 // Simple 404 error page.
 
-import { getSsrContext } from '@dr.pogodin/react-utils';
+import { getSsrContext, PageLayout } from '@dr.pogodin/react-utils';
 
 export default function Error404Page() {
   const ssrContext = getSsrContext(false);
   if (ssrContext) ssrContext.status = 404;
   return (
-    <h1>
-      Error 404: Requested resource is not found.
-    </h1>
+    <PageLayout>
+      <h1>Error 404: Requested resource is not found.</h1>
+    </PageLayout>
   );
 }
--- src/shared/pages/examples/Content.jsx
+++ src/shared/pages/examples/Content.jsx
@@ -10,16 +10,16 @@ export default function Content() {
       <h1>Dr. Pogodin&apos;s React Starter</h1>
       <ul>
         <li><Link to="api-client">API (HTTP) client</Link></li>
-        <li><Link to="async-global-data">Async Global Data</Link></li>
-        <li><Link to="code-splitting">Code Splitting</Link></li>
+        <li><Link to="async-global-data">Async global data</Link></li>
+        <li><Link to="code-splitting">Code splitting</Link></li>
         <li><Link to="buttons">Buttons</Link></li>
-        <li><Link to="global-state">Global State</Link></li>
-        <li><Link to="input-form-components">Input Form Components</Link></li>
-        <li><Link to="links">Link Tests</Link></li>
+        <li><Link to="global-state">Global state</Link></li>
+        <li><Link to="input-form-components">Input form components</Link></li>
+        <li><Link to="links">Link tests</Link></li>
         <li><Link to="modal">Modal</Link></li>
         <li><Link to="page-layout">Page layout</Link></li>
         <li><Link to="react-themes">React themes</Link></li>
-        <li><Link to="svg">SVG Demo</Link></li>
+        <li><Link to="svg">SVG demo</Link></li>
         <li><Link to="throbber">Throbber (activity indicator)</Link></li>
         <li><Link to="tooltip">Tooltip</Link></li>
         <li><Link to="youtube-video">YouTube video</Link></li>
