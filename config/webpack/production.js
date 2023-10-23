f27f772fce48927f1a0c2268cd2a581d6b5f6b9e
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2023-10-23 17:41:56 +0200

    react-utils@1.26.1

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,7 +1,9 @@
 const path = require('path');
+
 const standardConfigFactory = require(
   '@dr.pogodin/react-utils/config/webpack/app-production',
-);
+).default;
+
 const { merge } = require('webpack-merge');
 
 const customDefaultConfig = require('./default');
