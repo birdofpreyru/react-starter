a002c722e4d771435ccd51dedf4f9f4bfcacb37e
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2024-07-06 16:55:14 +0200

    [#33] Fixes router test

--- __tests__/shared/routes/index.tsx
+++ __tests__/shared/routes/index.tsx
@@ -1,9 +1,22 @@
 /** @jest-environment jsdom */
 
-import { snapshot } from '@dr.pogodin/react-utils/jest';
+import { MemoryRouter } from 'react-router-dom';
+import { mockClientSide, snapshot } from '@dr.pogodin/react-utils/jest';
 
-import Routes from 'routes';
-
-test('Matches shallow snapshot', () => {
-  snapshot(<Routes />);
+test('Matches shallow snapshot', async () => {
+  mockClientSide();
+  window.REACT_UTILS_INJECTION = {
+    CHUNK_GROUPS: {
+      'code-split-example': [],
+      'react-examples': [],
+    },
+  };
+  const { GlobalStateProvider, time } = require('@dr.pogodin/react-utils');
+  const Routes = require('routes').default;
+  await snapshot(
+    <GlobalStateProvider initialState={{}}>
+      <MemoryRouter><Routes /></MemoryRouter>
+    </GlobalStateProvider>,
+    { await: time.timer(0) },
+  );
 });
