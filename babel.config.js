1f15088a7b3ad44022b49c0db43fb783facf3acc
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2025-11-02 23:22:36 +0100

    react-utils@1.47.0-alpha.3

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
 /* global module */
 
 module.exports = {
+  plugins: [['babel-plugin-transform-import-meta', { module: 'ES6' }]],
   presets: [
     ['@dr.pogodin/react-utils/config/babel/node-ssr', {
       typescript: true,
--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,19 +1,7 @@
 /* Entry point that ensures correct ordering of imported external styles. */
 
-import type * as AppM from './app';
+import 'styles/global.scss';
 
-if (process.env.NODE_ENV === 'production') {
-  // eslint-disable-next-line import/no-unassigned-import, @typescript-eslint/no-require-imports
-  require('@dr.pogodin/react-utils/prod-styles');
-} else {
-  // eslint-disable-next-line import/no-unassigned-import, @typescript-eslint/no-require-imports
-  require('@dr.pogodin/react-utils/dev-styles');
-}
-
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-require('styles/global.scss');
-
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const App = (require('./app') as typeof AppM).default;
+import App from './app';
 
 export default App;
